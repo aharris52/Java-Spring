375d87ad847fda22856f8ec0b7b93c6834c71677
Andrew Harris <59749069+aharris52@users.noreply.github.com>
2021-04-15 21:10:39 -0700

    Add files via upload
    Created a Spring Boot web app with custom configuration data (non-secure in this demo) as well as changing trivial configuration parameters.

---
+++ Configuration Example/src/main/java/com/whiteriverdev/sdev/demo/ConfigurationExampleApplication.java
@@ -0,0 +1,17 @@
+package com.whiteriverdev.sdev.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+* demo for application properties configuration
+* */
+
+@SpringBootApplication
+public class ConfigurationExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationExampleApplication.class, args);
+    }
+
+}
